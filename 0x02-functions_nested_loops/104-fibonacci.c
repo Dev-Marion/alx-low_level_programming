983e148abff875309e5893022d37ef03dfd83be4
Marion <marionmwangi007@gmail.com>
2022-04-27 13:42:30 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Entry point, prints the first 50 fibonacci numbers
+ * main - Entry point, prints the first 98 Fibonacci numbers
  *
  * Return: Always 0
  */
 int main(void)
 {
 	int counter = 0;
-	unsigned long first = 0;
-	unsigned long second = 1;
+
+	unsigned long first = 1;
+
+	unsigned long second = 2;
+
 	unsigned long fib;
 
 	while (counter <= 97)
 	{
 		fib = first + second;
+
 		if (counter < 97)
 		{
 			printf("%lu, ", fib);
@@ -30,11 +34,8 @@ int main(void)
 		second = fib;
 
 		counter++;
-
 	}
-
 	printf("\n");
 
 	return (0);
-
 }
