71bd10b58b4065e5b3302e71df3d61e23ee0a7a4
Marion <marionmwangi@gmail.com>
2022-07-13 12:23:57 -0700

    Removed spaces

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,22 +10,22 @@ int main(void)
 	int counter = 2;
 
 	float a = 1;
-        float b = a + 1;
-        float c = a + b;
+	float b = a + 1;
+	float c = a + b;
 
-        printf("%.0f, ", a);
-        printf("%.0f, ", b);
+	printf("%.0f, ", a);
+	printf("%.0f, ", b);
 	while (counter < 98)
 	{
 		counter++;
-                printf("%.0f", c);
-                a = b;
-                b = c;
-                c = a + b;
+		printf("%.0f", c);
+		a = b;
+		b = c;
+		c = a + b;
 		if (counter < 98)
 		{
 			printf(", ");
-		}	
+		}
 	}
 	printf("\n");
 
